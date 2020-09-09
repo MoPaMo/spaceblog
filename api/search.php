b798bcaa5dc9d4515d6f6df49965f6e6353504ab
Glitch (lamp-poc) <none>
2020-09-09 06:18:50 +0000

    🎐🔑 Checkpoint ./search.php:11290180/6176 ./api/search.php:11290180/1365 ./.glitch-assets:11290180/454

---
+++ api/search.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_GET["name"])){
+include("../db.php");
+$conn=db();
+  $res1=$conn->query("SELECT * FROM posts WHERE  title LIKE \"%".bin2hex($_GET["name"])."%\" ORDER BY created desc;");
+  $res2=$res1->fetch_all(MYSQLI_ASSOC);
+  if(count($res2>0)){
+      foreach($res2 as $res){ ?>
+          <div class="post-preview">
+            <a href="/article/<?php echo $res["id"];?>">
+              <h2 class="post-title"><?php echo hex2bin($res["title"]);?></h2>
+              <h3 class="post-subtitle"><?php echo hex2bin($res["short"]);?></h3>
+            </a>
+            <p class="post-meta">
+              Posted by
+              <a href="/author/<?php echo hex2bin($res["author"]);?>"><?php echo hex2bin($res["author"]);?></a>
+              on <?php echo $res["created"];?>
+            </p>
+          </div>
+          <hr /><?php }else{echo "<img src="https://cdn.glitch.com/637778d7-facd-4553-820b-773fd6182020%2Fundraw_empty_xct9.svg?v=1599632299973">"}}} ?>
